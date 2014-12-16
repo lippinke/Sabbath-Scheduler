61b5359510e6b57b7b9dabf8ed452802555a59d7
bowiec <bowiec@onid.oregonstate.edu>
2014-12-16 14:55:52 -0800

    stuff was moved here

---
+++ php/person_api.php
@@ -0,0 +1,35 @@
+<?php
+	require_once 'functions.php';
+
+	function get_person($db, $person_id)
+	{
+		$query = "SELECT *
+		          FROM person p
+		          WHERE p.id = $person_id";
+
+		$result = mysqli_query($db, $query);
+
+		return '{"person":' . result_to_json($result) . '}';
+	}
+
+	function add_person($db, $first, $last, $email, $password)
+	{
+		$data = array(
+		             "first"    => $first,
+		             "last"     => $last,
+		             "email"    => $email,
+		             "password" => $password
+		            );
+
+		return insert_row($db, "person", $data);
+	}
+
+	function check_admin($person_id, $db){
+		if (!is_int($person_id)){
+			return;
+		}
+		$data = mysqli_query($db, "SELECT congregation FROM admin
+			WHERE person = $person_id");
+		return result_to_json($data);
+	}
+?>
