6f2257e0b13943153e7add65fdfbcabf55abfe67
bowiec <bowiec@onid.oregonstate.edu>
2014-12-16 12:26:13 -0800

    cleaned up

--- person_queries.php
+++ person_queries.php
@@ -1,12 +1,4 @@
 <?php
-	require_once 'config.php';
-	require_once 'db_connect.php';
-
-	$db = db_connect();
-
-	echo get_person($db, 1);
-	echo get_person($db, 2);
-
 	function get_person($db, $person_id)
 	{
 		$query = "SELECT *
@@ -15,11 +7,6 @@
 
 		$result = mysqli_query($db, $query);
 
-		$rows = array();
-		while($record = mysqli_fetch_assoc($result)){
-			$rows[] = $record;
-		}
-
-		return json_encode($rows);
+		return result_to_json($result);
 	}
 ?>
