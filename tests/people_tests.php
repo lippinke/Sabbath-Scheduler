b954193f3a107680e5e5a6e1f503ec5d7f3fa165
bowiec <bowiec@onid.oregonstate.edu>
2014-12-16 14:46:22 -0800

    moved sschedulerAPI.php

--- tests/people_tests.php
+++ tests/people_tests.php
@@ -1,9 +1,12 @@
 <?php
 	require_once '../php/db_connect.php';
 	require_once '../php/people_queries.php';
+	require_once '../php/sschedulerAPI.php';
 
 	$db = db_connect();
 
-	echo get_person($db, 1);
+	echo get_person($db, 1)."\n";
+
+	echo add_person($db, "keith", "keith", "keith@keith.keith", "keith");
 
 ?>
