6af2c9e4a7157786e56c19c0db8d15a5aabde61a
bowiec <bowiec@onid.oregonstate.edu>
2014-12-16 12:59:51 -0800

    reogranize files

--- php/functions.php
+++ php/functions.php
@@ -26,27 +26,4 @@
 			return FALSE;
 		}
 	}
-
-	function get_person($db, $person_id)
-	{
-		$query = "SELECT *
-		          FROM person p
-		          WHERE p.id = $person_id";
-
-		$result = mysqli_query($db, $query);
-
-		return result_to_json($result);
-	}
-
-	function add_person($db, $first, $last, $email, $password)
-	{
-		$data = array(
-		             "first"    => $first,
-		             "last"     => $last,
-		             "email"    => $email,
-		             "password" => $password
-		            );
-
-		return insert_row($db, "person", $data);
-	}
 ?>
---
+++ php/people_queries.php
@@ -0,0 +1,24 @@
+<?php
+	function get_person($db, $person_id)
+	{
+		$query = "SELECT *
+		          FROM person p
+		          WHERE p.id = $person_id";
+
+		$result = mysqli_query($db, $query);
+
+		return result_to_json($result);
+	}
+
+	function add_person($db, $first, $last, $email, $password)
+	{
+		$data = array(
+		             "first"    => $first,
+		             "last"     => $last,
+		             "email"    => $email,
+		             "password" => $password
+		            );
+
+		return insert_row($db, "person", $data);
+	}
+?>
