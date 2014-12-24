7cf038c7ed6f2518fc65f3880577d971b231e897
Godfrey Yeung <godfreyyeung@gmail.com>
2014-12-24 14:46:26 -0800

    rearranged views

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+<?
 	require_once 'config/config.php';
 	require_once 'php/db_connect.php';
 ?>
@@ -8,34 +8,36 @@
 		<title><?php echo $title; ?></title>
 		<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 		<link rel="stylesheet" type="text/css" href="styles/main.css">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="js/uuser.js"></script>
 	</head>
 
 	<body>
 	    <div id="outer-wrap">
 			<h1>Sabbath Scheduler</h1>
 
-			<a href=""><div class="button user"> 
+			<a href="" class="btnlink"><div class="button userbtn"> 
 						View Sabbath
 						</div></a>
-			<a href=""><div class="button user"> 
+			<a href="" class="btnlink"><div class="button userbtn"> 
 						View Calendar
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						Add Event
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						View People
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						View Congregations
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						View Roles
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						View Saved Locations
 						</div></a>
-			<a href=""><div class="button admin"> 
+			<a href="" class="btnlink"><div class="button adminbtn"> 
 						View Templates
 						</div></a>
 
